1c893f22cc8915d91defad382fc480115cef360a
techmannih <manishchaudharyttt@gmail.com>
2024-04-06 17:03:12 +0530

    get messages functions added

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import Conversation from "../models/conversation.modal.js";
 import Message from "../models/message.model.js";
 
+//send message
 export const sendMessage = async (req, res) => {
     try {
         const { message } = req.body;
@@ -24,9 +25,18 @@ export const sendMessage = async (req, res) => {
             message,
         });
 
-        conversation.messages.push(newMessage._id);
-        await conversation.save();
-        await newMessage.save();
+        if(newMessage){
+            conversation.messages.push(newMessage._id);
+
+        }
+
+        //Socket.io functionality will be added here
+
+        
+        //await conversation.save();
+        //await newMessage.save();
+        // Instead of saving the conversation and message separately, we can save them together using Promise.all
+        await Promise.all([conversation.save(), newMessage.save()]);
 
         res.status(200).json({ message: "New Message sent successfully" });
 
@@ -34,4 +44,25 @@ export const sendMessage = async (req, res) => {
         console.error("Error in sendMessage controller: ", error.message);
         res.status(500).send({ error: "Internal server error" });
     }
+};
+
+//get messages
+export const getMessages = async (req, res) => {
+	try {
+		const { id: userToChatId } = req.params;
+		const senderId = req.user._id;
+
+		const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+
+		if (!conversation) return res.status(200).json([]);
+
+		const messages = conversation.messages;
+
+		res.status(200).json(messages);
+	} catch (error) {
+		console.log("Error in getMessages controller: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
 };
