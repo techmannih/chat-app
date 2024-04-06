85caa3d44ca005a9609393634474203639511700
techmannih <manishchaudharyttt@gmail.com>
2024-04-06 17:40:05 +0530

    Initial backend part completed

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -15,7 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes)
 app.use('/api/messages',messageRoutes);
-
+app.use('/api/users',userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB()
