2cc5f567a032d113d7b6d0263800dc404eacd019
techmannih <manishchaudharyttt@gmail.com>
2024-04-07 15:18:35 +0530

    components created for frontend

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+
+import SearchInput from './SearchInput'
+
+const Sidebar = () => {
+  return (
+    <div>
+        <SearchInput/>
+        <div className="devider px-3"></div>
+        {/* <Conversations/>
+        <LogoutButton/> */}
+
+    </div>
+  )
+}
+
+export default Sidebar
