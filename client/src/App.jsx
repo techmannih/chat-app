a78f020aff37b2263df779b9220f016a362d3b0a
techmannih <manishchaudharyttt@gmail.com>
2024-04-07 23:49:28 +0530

    add react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Login from "../src/pages/login/Login"
 import SignUp from "../src/pages/signup/SignUp"
 import Home from "./pages/home/Home"
+import { Routes, Route } from "react-router-dom"
 
 
 
@@ -9,11 +10,11 @@ function App() {
 
   return (
    <div className="p-4 h-screen flex items-center justify-center  ">
-    {/* <Login />*/}
-    <Home /> 
-   {/*  <SignUp /> */}
-
-   
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
    </div>
   )
 }
