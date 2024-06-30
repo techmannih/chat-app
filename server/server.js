dadd1e0c66ad6e361b51e37f61e6cda2787d656c
techmannih <manishchaudharyttt@gmail.com>
2024-07-01 00:18:39 +0530

    add cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-// server.js
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -11,25 +10,27 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server  } from './socket/socket.js';
 
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
-dotenv.config();
+// Middleware
+app.use(cors());
 app.use(express.json()); // to parse the incoming request with JSON payloads from req.body
 app.use(cookieParser()); 
 
-app.use("/api/auth",authRoutes)
-app.use('/api/messages',messageRoutes);
-app.use('/api/users',userRoutes);
+// Routes
+app.use("/api/auth", authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get("/", (req, res) => {
+  res.send('Welcome to the chat app');
+}
+);
 
-app.get('*', (req, res) => {res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
-});
 
 server.listen(PORT, () => {
-  connectToMongoDB()
-  console.log(`Server is running on ports ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
 });
